a18af818278d54dd7eaed3d8fbdf9bd8bbc04d84
Nazarii <nazar.volitskii@gmail.com>
2022-09-29 15:37:36 +0300

    app

--- memory_box/android/app/src/main/kotlin/com/example/memory_box/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.memory_box
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- memory_box/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:memory_box/pages/auth_page.dart';
-import 'package:memory_box/pages/loading_page.dart';
-
-void main() {
-  runApp(const MaterialApp(home: HelloPageIfNew()));
-}
